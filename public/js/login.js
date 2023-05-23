3845cecce25703f3102b539a967d0520172064d4
hendrikjp24 <hendrikzerojp@gmail.com>
2023-05-23 20:27:22 +0700

    Memperbaiki bug pada ui login, tepatnya pada alert di bawah bt login.

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,6 @@
 const btlogin = document.getElementById("btlogin");
 
-console.log(btlogin);
-
-
 if(document.location.href.includes("login")){
-    console.log("INI PAGE LOGIN");
     document.body.style.backgroundColor = "#3eb2f0";
     document.body.style.backgroundImage = "linear-gradient(to bottom right, #3eb2f0, #fafcfc)";
-}else{
-    console.log("INI BUKAN PAGE LOGIN");
 }
